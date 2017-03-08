060903a314df2d239b43002f5a9c95dd4d69c210
alicehly <alicehly@gmail.com>
2017-03-07 22:28:10 -0500

    Add files via upload

--- foods.js
+++ foods.js
@@ -1,14 +1,14 @@
      // A $( document ).ready() block.
 $( document ).ready(function() {
-    
+
 
     console.log("ready!");
 
 
       google.charts.load('current', {'packages':['bar']});
-      google.charts.setOnLoadCallback(drawFood);
+      google.charts.setOnLoadCallback(drawStuff);
 
-      function drawFood() {
+      function drawStuff() {
         var data = new google.visualization.arrayToDataTable([
           ['Food', 'NY, NY', 'Bronx, NY'],
           ['Milk (1 G)', '$4.02', '$3.18'],
@@ -16,21 +16,21 @@ $( document ).ready(function() {
           ['White Rice (1LB)', '$2.72', '$2.00'],
           ['Eggs', '$3.46', '$2.12'],
           ['Chicken Breast (1LB)', '$5.14', '$3.00'],
-          ['Beef (1LB)', '$6.16', '$5.17'],
+          ['Beef Round (1LB)', '$6.16', '$5.17'],
           ['Apples (1LB)', '$2.65', '$1.50'],
           ['Potatos (1LB)', '$1.53', '$.63'],
           ['Lettuce', '$2.08', '$2.00'],
         ]);
 
         var options = {
-          width: 1000,
+          width: 900,
           chart: {
-            title: 'Costs of Living Prices in Manhattan, NY vs. Bronx, NY',
-            subtitle: 'Source: 2009-2017 Numbeo'
+            title: 'Average Grocery Prices in NYC',
+            subtitle: 'Market Food Prices in NY, NY vs. Bronx, NY'
           },
           series: {
-            0: { axis: 'NY, NY' }, // Bind series 0 to an axis named 'NY, NY'.
-            1: { axis: 'Bronx, NY' } // Bind series 1 to an axis named 'Bronx, NY'.
+            0: { axis: 'NY, NY' }, // Bind series 0 to an axis named 'distance'.
+            1: { axis: 'Bronx, NY' } // Bind series 1 to an axis named 'brightness'.
           },
           axes: {
             y: {
@@ -40,51 +40,18 @@ $( document ).ready(function() {
           }
         };
 
-      var chart = new google.charts.Bar(document.getElementById('FoodChart'));
+         var chart = new google.charts.Bar(document.getElementById('dual_y_div'));
       chart.draw(data, options);
     };
 
+  $("#slideshow > div:gt(0)").hide();
 
-    google.charts.setOnLoadCallback(drawOtherStuff);
-
-     function drawOtherStuff() {
-        var data = new google.visualization.arrayToDataTable([
-          ['Country','Crop Water Requirement', 'Crop water consumed (mm)'],
-          ['Argentina', 877, 877],
-          ['Australia', 901, 843],
-          ['Brazil', 606, 551],
-          ['China', 718, 638],
-          ['Egypt', 1009, 1009],
-          ['Greece', 707, 707],
-          ['India', 810, 538],
-          ['Mali', 993, 538],
-          ['Mexico', 771, 746],
-          ['Pakistan', 850, 850],
-          ['Syria', 1309, 1309],
-          ['Turkey', 963, 963],
-          ['Turkmenistan', 1025, 1025],
-          ['USA', 516, 419],
-          ['Uzbekistan', 999, 999],
-        ]);
-
-        var options = {
-          width: 900,
-          chart: {
-            title: ' Consumptive water use at field level for cotton production in the major cotton producing countries. ',
-            subtitle: ' Sources: Gillham et al. (1995); FAO (1999); Cotton Australia (2005); CCI (2005); WWF (1999). '
-          },
-          series: {
-            1: { axis: 'water' } 
-          },
-          axes: {
-            y: {
-
-              water: {side: 'left', label: 'Water Consumed (mm)'} // Right y-axis.
-            }
-          }
-      };
-
-        var chartTwo = new google.charts.Bar(document.getElementById('secondChart'));
-      chartTwo.draw(data, options);
-  };
-  });
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(1050)
+    .next()
+    .fadeIn(1050)
+    .end()
+    .appendTo('#slideshow');
+}, 10000);
+});
